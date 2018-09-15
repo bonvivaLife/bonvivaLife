099ac2acdb982cb69c9aa517f1c3884b641764a0
Andreas Steger <norelpy>
2018-09-15 00:45:24 +0000

    Add set_state

--- server/main.py
+++ server/main.py
@@ -1,8 +1,10 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_restful import Resource, Api
+from flask_cors import CORS
 import data
 
 app = Flask(__name__)
+CORS(app)
 api = Api(app)
 
 class HelloWorld(Resource):
@@ -12,6 +14,20 @@ class HelloWorld(Resource):
 class items(Resource):
     def get(self):
         return d
+
+@app.route('/set_state/<user_id>/<state>', methods = ['POST'])
+def user(user_id, state):
+    user_id = int(user_id)
+
+    found = False
+    for index in range(len(d)):
+        if d[index]['id'] == user_id:
+            d[index]['status'] = state
+            found = True
+
+    if not found:
+        raise RuntimeError("ID not Found")
+    return jsonify(d)
 
 api.add_resource(HelloWorld, '/')
 api.add_resource(items, '/items')
